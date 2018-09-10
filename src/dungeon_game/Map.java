c711006a7ac865ad2c346a7e78ab8d64c14081b3
Andrew Kernahan <z5164699@student.unsw.edu.au>
2018-09-10 16:52:24 +1000

    bug fixed, now Map.java will print map on terminal with letters standing for tiles

--- src/dungeon_game/Map.java
+++ src/dungeon_game/Map.java
@@ -7,13 +7,14 @@ public class Map {
 	public Map(int size) {
 		this.mapsize = size;
 		map = new MapTile[size][size];
-		//this.addBoundries(); // puts walls on every tile that is on the edge
+		this.addTiles(); // creates MapTile objects and puts Walls on edges
 	}
 	
 	// adds walls to all MapTiles on the edge of the 2d array
-	public void addBoundries() {
+	public void addTiles() {
 		for (int i = 0; i < mapsize; i++) {
 			for (int j = 0; j < mapsize; j++) {
+				map[i][j] = new MapTile(i, j);
 				if (i == 0 || j ==0 || i == mapsize-1 || j == mapsize-1) {
 					Terrain w = new Wall();
 					map[i][j].addTerrain(w);
@@ -28,9 +29,9 @@ public class Map {
 		for (int i = 0; i < mapsize; i++) {
 			for (int j = 0; j < mapsize; j++) {
 				if (map[i][j].hasTerrain())
-					System.out.print("T");
+					System.out.print("T ");
 				else
-					System.out.print("F");
+					System.out.print("F ");
 			}
 			System.out.print("\n");
 		}
@@ -39,10 +40,10 @@ public class Map {
 	// main function only for early testing and debugging
 	public static void main(String[] args) {
 		Map m = new Map(8);
+		System.out.println(m.mapsize);
 		Terrain t = new Wall();
-		m.map[0][0].addTerrain(t);
-		//m.addBoundries();
-		//m.printOnTerminal();
+		//m.map[0][0].addTerrain(t);
+		m.printOnTerminal();
 	}
 	
 }
