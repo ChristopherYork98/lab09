d53ec7b762698ea3bed4e4d6958ddc690402dc8e
yihenghu317 <z5111471@student.unsw.edu.au>
2018-09-16 01:24:28 +1000

    add attributes and methods

--- src/dungeon_game/PlayerCharacter.java
+++ src/dungeon_game/PlayerCharacter.java
@@ -2,9 +2,34 @@ package dungeon_game;
 
 public class PlayerCharacter {
 	private Item Inhand;
-	private Inventory inventory;
-	 
+	private Inventory Inventory;
+	private String direction;
+	private boolean invincible;
+	private boolean hovering;
+	private int currentTreasure;
 	
+	public PlayerCharacter(int inventoryspace) {
+		this.Inhand = null;
+		this.Inventory = new Inventory(inventoryspace);
+		this.invincible = false;
+		this.hovering = false;
+		this.currentTreasure = 0;
+	}
+	
+	public Item pickupItem(Item item) {
+		return this.Inventory.add_item(item);
+	}
+	
+	public void dropItem(Item item) {
+		this.Inventory.remove_item(item);
+	}
+	
+	public void putInhand(Item item) {
+		this.Inhand = item;
+		this.Inventory.remove_item(item);
+	}
+	
+
 	
 	
 }
